d186d2e0017b146787b13e123726941337134d07
andreagori <115431777+andreagori@users.noreply.github.com>
2025-01-22 23:26:18 +0000

    adding portfolio projects

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -8,14 +8,33 @@
  * as you continue to learn and create.
  */
 
-import React from "react";
+import React, {useState} from "react";
 
 /**
- * Desk image
+ * Carrousel Images:
  */
-import image from "../images/roman-synkevych-vXInUOv1n84-unsplash.jpg";
+import image1 from "../images/projects/cuatasWebsite.png";
+import image2 from "../images/projects/Estilist.png";
+import image3 from "../images/projects/CodeFront.png";
+import image4 from "../images/projects/PetCare.png";
+import image5 from "../images/projects/MiawChess.png";
+import image6 from "../images/projects/Imjustgirl.png";
+import image7 from "../images/projects/signmatch.png";
 
-const imageAltText = "desktop with books and laptop";
+const images = [
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+];
+
+const imageAltText = [
+  "CUATAS Project",
+  "Resume Site",
+];
 
 /**
  * Project list
@@ -39,48 +58,111 @@ const projectList = [
   {
     title: "Estilist",
     description:
-      "...",
-    url: "",
+      "Fashion discovery platform offering personalized style recommendations based on user preferences and colorimetry",
+    url: "https://github.com/estilist/",
   },
   {
     title: "CodeFront",
     description:
-      "...",
-    url: "",
+      "Website deploy application that allows users to fetch and view news articles from multiple online sources in one place.",
+    url: "https://github.com/keevin-21/CodeFront/tree/andrea",
   },
   {
     title: "PetCare",
     description:
-      "...",
-    url: "",
+      "Pet information management web Application.",
+    url: "https://github.com/andreagori/PetCare",
+  },
+  {
+    title: "MeawChess",
+    description:
+      "Chess game using OPP with customized cat-themed",
+    url: "https://github.com/andreagori/MiawChess",
+  },
+  {
+    title: "Imjustgirl Chatbot",
+    description:
+      "The chatbot will ask the user some questions about their preferences and then it will show them some nails designs that they might like.",
+    url: "https://andreagori.github.io/chatbot_imjustgirl/",
   },
   {
     title: "SignMatch",
     description:
-      "...",
-    url: "",
+      "Educational memory game for learning sign language.",
+    url: "https://github.com/andreagori/signmatchgame",
   },
 ];
 
 const Portfolio = () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const handleNext = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const handlePrev = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
   return (
     <section className="padding" id="portfolio" style={{ backgroundColor: "rgb(14, 14, 16)" }}>
       <h2 style={{ textAlign: "center", color: "white" }}>Portfolio</h2>
       <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
-        <div style={{ maxWidth: "40%", alignSelf: "center" }}>
+        {/* Carrusel de imágenes */}
+        <div style={{ maxWidth: "40%", alignSelf: "center", position: "relative", marginLeft: "3rem" }}>
           <img
-            src={image}
-            style={{ height: "90%", width: "100%", objectFit: "cover" }}
-            alt={imageAltText}
+            src={images[currentImageIndex]}
+            alt={imageAltText[currentImageIndex]}
+            style={{ height: "90%", width: "100%", objectFit: "cover", borderRadius: "10px"}}
           />
+          <button
+            onClick={handlePrev}
+            style={{
+              position: "absolute",
+              top: "50%",
+              left: "10px",
+              transform: "translateY(-50%)",
+              backgroundColor: "rgba(0, 0, 0, 0.5)",
+              color: "white",
+              border: "none",
+              borderRadius: "50%",
+              width: "30px",
+              height: "30px",
+              cursor: "pointer",
+            }}
+          >
+            ❮
+          </button>
+          <button
+            onClick={handleNext}
+            style={{
+              position: "absolute",
+              top: "50%",
+              right: "10px",
+              transform: "translateY(-50%)",
+              backgroundColor: "rgba(0, 0, 0, 0.5)",
+              color: "white",
+              border: "none",
+              borderRadius: "50%",
+              width: "30px",
+              height: "30px",
+              cursor: "pointer",
+            }}
+          >
+            ❯
+          </button>
         </div>
+
+        {/* Lista de proyectos */}
         <div className="container">
           {projectList.map((project) => (
             <div className="box" key={project.title}>
               <a href={project.url} target="_blank" rel="noopener noreferrer">
                 <h3 style={{ flexBasis: "40px" }}>{project.title}</h3>
               </a>
-              <p className="small" style={{color:"white"}}>{project.description}</p>
+              <p className="small" style={{ color: "white" }}>{project.description}</p>
             </div>
           ))}
         </div>
